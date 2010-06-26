76fa032543d27d407ddbdffe551a0bf1f8cdf9c6
benn.bollay <benn.bollay@gmail.com>
2010-06-26 06:12:11 +0000

    Basic checkin of the three demonstration Eclipse projects.

---
+++ Demo liblua/src/org/lua/demo/LibluaDemo.java
@@ -0,0 +1,17 @@
+package org.lua.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+ * This does absolutely nothing, and is just a placeholder to
+ * give something for the test project to, well, test.
+ */
+public class LibluaDemo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
